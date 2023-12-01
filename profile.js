2f4128c06b4671a0a2d612b71495d707c9ab58ba
Shattuck Kuzmic <shattuckkuzmic@gmail.com>
2023-12-01 12:29:36 -0800

    Assessment Done

--- profile.js
+++ profile.js
@@ -27,3 +27,26 @@ placeBtn.addEventListener('click', placeGen)
 ritualBtn.addEventListener('click', ritualGen)
 
 skillBtn.textContent = ('Click to View')
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// controls next/previous slides
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// function controls thumbnail
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
